ccb94d64258e1b73ead0548e6c5da89b3835b447
정현진 <hyeonjin0905@inzent.com>
2024-07-16 11:47:38 +0900

    주문과 할인 정책 실행 코드 및 테스트 코드 추가

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderSerivce orderService = new OrderServiceImpl();
+
+    @Test
+    public void createOrder() {
+        //long memberId 로 하면 null이 안들어가서 일단 Long 타입을 사용
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertEquals(order.getDiscountPrice(), 1000);
+        Assertions.assertEquals(order.calculatePrice(), 9000);
+    }
+}
