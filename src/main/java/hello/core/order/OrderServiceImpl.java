d5e9d41640db5d754deb77634a9e755c710e8d2e
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-07-24 20:51:06 +0900

    할인 정책 DIP, OCP위반된 코드를 인터페이스에만 의존하도록 변경

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,13 +10,19 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderSerivce{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     //할인 정책을 변경하려면 코드를 변경해야함
     //DIP 위반 : 구체화에 의존하지말고 추상화에 의존하라를 위반한다 추상(인터페이스)뿐만 아니라 구체(구현)클래스에도 의존하고 있다.
     //추상 (인터페이스)의존 : DiscountPolicy / 구체(구현)클래스 : FixDiscountPolicy, RateDiscountPolicy
     //OCP위반 : fixDiscountpolicy를 ratediscountpolicy로 변경하는 순간 orderservieimpl의 소스도 함께 변경해야함
 
     //해결 : DIP를 위반하지 않도록 인터페이스에만 의존하도록 의존관계를 변경하면 된다.
+
+
+    private DiscountPolicy discountPolicy; //인터페이스에만 의존하도록 변경
+    // 이 대로만 호출하면 NPE 발생
+
+    //해결 : 누군가가 클라이언트인 orderServiceImpl에 DiscoutnPolicy의 구현 객체를 대신 생성하고 주입해주어야한다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
