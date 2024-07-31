ecbf6296042a00b600e7d3cbb81353080f139965
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-07-31 20:32:36 +0900

    테스트

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -9,19 +9,19 @@ import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderSerivce orderService = new OrderServiceImpl();
-
-    @Test
-    public void createOrder() {
-        //long memberId 로 하면 null이 안들어가서 일단 Long 타입을 사용
-        Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
-        memberService.join(member);
-
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-
-        Assertions.assertEquals(order.getDiscountPrice(), 1000);
-        Assertions.assertEquals(order.calculatePrice(), 9000);
-    }
+//    MemberService memberService = new MemberServiceImpl();
+//    OrderSerivce orderService = new OrderServiceImpl();
+//
+//    @Test
+//    public void createOrder() {
+//        //long memberId 로 하면 null이 안들어가서 일단 Long 타입을 사용
+//        Long memberId = 1L;
+//        Member member = new Member(memberId, "memberA", Grade.VIP);
+//        memberService.join(member);
+//
+//        Order order = orderService.createOrder(memberId, "itemA", 10000);
+//
+//        Assertions.assertEquals(order.getDiscountPrice(), 1000);
+//        Assertions.assertEquals(order.calculatePrice(), 9000);
+//    }
 }
