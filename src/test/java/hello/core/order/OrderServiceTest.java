f8ba278bbef797021607fabc4bbecac6b1800218
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-07-31 20:42:16 +0900

    appConfig 리팩토링

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,27 +1,36 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-//    MemberService memberService = new MemberServiceImpl();
-//    OrderSerivce orderService = new OrderServiceImpl();
-//
-//    @Test
-//    public void createOrder() {
-//        //long memberId 로 하면 null이 안들어가서 일단 Long 타입을 사용
-//        Long memberId = 1L;
-//        Member member = new Member(memberId, "memberA", Grade.VIP);
-//        memberService.join(member);
-//
-//        Order order = orderService.createOrder(memberId, "itemA", 10000);
-//
-//        Assertions.assertEquals(order.getDiscountPrice(), 1000);
-//        Assertions.assertEquals(order.calculatePrice(), 9000);
-//    }
+    MemberService memberService;
+    OrderSerivce orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderSerivce();
+    }
+
+    @Test
+    public void createOrder() {
+        //long memberId 로 하면 null이 안들어가서 일단 Long 타입을 사용
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertEquals(order.getDiscountPrice(), 1000);
+        Assertions.assertEquals(order.calculatePrice(), 9000);
+    }
 }
